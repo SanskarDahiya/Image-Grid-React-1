e22e5ced3f3c273111bd21b5cd74e51f549de08f
Sanskar Dahiya <sanskardahiya98@gmail.com>
2022-05-20 11:06:19 +0530

    update error message

--- src/Apis/ImageApi.js
+++ src/Apis/ImageApi.js
@@ -2,7 +2,7 @@ import axiosHooks from "axios-hooks";
 import Axios from "axios";
 const REACT_APP_UNSPLASH_ACCESS_TOKEN = process.env.REACT_APP_UNSPLASH_ACCESS_TOKEN;
 if (!REACT_APP_UNSPLASH_ACCESS_TOKEN) {
-  alert("No Access Token Available");
+  alert("No Access Token Available\nPlease copy sampleEnv to .env file and try again!!");
 }
 // https://api.unsplash.com/photos/er9wYHRk55Y?page=1&client_id=ACCESS_TOKEN
 const axios = Axios.create({
